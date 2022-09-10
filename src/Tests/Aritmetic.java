adbc0d665591c9ead9f2e048d9aeae33e09d1e4c
Troels Lund <trolund@gmail.com>
2022-09-10 14:10:28 +0200

    more testing and can now create data dumps

---
+++ src/Tests/Aritmetic.java
@@ -0,0 +1,121 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Aritmetic {
+
+    private ProgramLoader loader = new ProgramLoader();
+
+    @Test
+    public void add(){
+
+
+        ISASimulator vm = new ISASimulator();
+
+        // test case
+        vm.runProgram(loader.loadTest("add"));
+
+        int[] reg = vm.getReg();
+
+        assertEquals(5, reg[3]);
+    }
+
+    @Test
+    public void addi(){
+
+        ISASimulator vm = new ISASimulator();
+
+        // test case
+        vm.runProgram(loader.loadTest("addi"));
+
+        int[] reg = vm.getReg();
+
+        assertEquals(42, reg[11]);
+
+    }
+
+    @Test
+    public void sub(){
+
+        ISASimulator vm = new ISASimulator();
+
+        // test case
+        vm.runProgram(loader.loadTest("sub"));
+
+        int[] reg = vm.getReg();
+
+        assertEquals(41, reg[11]);
+
+    }
+
+    @Test
+    public void slt(){
+
+        ISASimulator vm = new ISASimulator();
+
+        // test case
+        vm.runProgram(loader.loadTest("slt"));
+
+        int[] reg = vm.getReg();
+
+        assertEquals(0, reg[11]);
+
+    }
+
+    @Test
+    public void slt2(){
+
+        ISASimulator vm = new ISASimulator();
+
+        // test case
+        vm.runProgram(loader.loadTest("slt2"));
+
+        int[] reg = vm.getReg();
+
+        assertEquals(1, reg[11]);
+
+    }
+
+    @Test
+    public void slti(){
+
+        ISASimulator vm = new ISASimulator();
+
+        // test case
+        vm.runProgram(loader.loadTest("slti"));
+
+        int[] reg = vm.getReg();
+
+        assertEquals(1, reg[11]);
+
+    }
+
+    @Test
+    public void sltu(){
+
+        ISASimulator vm = new ISASimulator();
+
+        // test case
+        vm.runProgram(loader.loadTest("sltu"));
+
+        int[] reg = vm.getReg();
+
+        assertEquals(1, reg[12]);
+
+    }
+
+    @Test
+    public void sltiu(){
+
+        ISASimulator vm = new ISASimulator();
+
+        // test case
+        vm.runProgram(loader.loadTest("sltiu"));
+
+        int[] reg = vm.getReg();
+
+        assertEquals(1, reg[12]);
+
+    }
+
+}
