eb0c96585f8ff316986b2bd3bf8887c8531089f0
Troels Lund <trolund@gmail.com>
2022-09-28 14:45:05 +0200

    min_max function done

--- assignments/min_max.s
+++ assignments/min_max.s
@@ -1,5 +1,5 @@
 .test_data:
-	li t4 0x100000 # memmory start
+li t4 0x100000 # memory pointer start at 0x100000
     # test values
     li t6 2 # load intermited to t6
     sw t6 0(t4) # store t6 in memory 
@@ -11,24 +11,28 @@
     sw t6 12(t4)
     li t6 8
     sw t6 16(t4)
-    li t6 3
+    li t6 -3
     sw t6 20(t4)
-    li t6 1
+    li t6 -1
     sw t6 24(t4)
-.max:    
-    li t4 0x100000 # memmory start at 0x100000
+
+
+
+
+.min_max:    
+    li t4 0x100000 # memory pointer start at 0x100000
 
     lw 		t0 0(t4)    # t0 = temp max 
     lw 		t1 0(t4)    # t1 = temp min 
-    li      t2, 6 	    # number of loop's (length in memory - 1)
-    li 		t3, 0		# counter vaiable
+    li         t2, 99 	     # number of loop's (length in memory - 1)
+    li 		t3, 0	     # counter variable
     
     loop_head:
     blt     t2, t3, loop_end  # run loop while t2 < t3 (t2 == 101)
     
     # Repeated code goes here
     lw 	t5 0(t4)		# load value from memory 
-    ADDI t4 t4 4		# add 4 to memmory pointer    
+    ADDI t4 t4 4		# add 4 to memory pointer    
     ADDI t3 t3 1 		# add 1 to t3 (to end loop at a 100)
     
     # check max
@@ -47,4 +51,3 @@
     # return data
     ADD a0 zero t1 # set a0 to the minimum value 
     ADD a1 zero t0 # set a01 to the maximum value 
-    
