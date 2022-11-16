aa2d85100490816e021c29fb049cc4acfb7f3f96
Troels Lund <trolund@gmail.com>
2022-11-16 08:31:02 +0100

    added all task 4 test and they all pass

---
+++ src/Tests/Task4.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Task4 {
+
+    private final TestUtil test = new TestUtil();
+
+    @Test
+    public void t1() throws IOException {
+        assertTrue(test.runTestWithName(4, "t1"));
+    }
+
+
+    @Test
+    public void t2() throws IOException {
+        assertTrue(test.runTestWithName(4, "t2"));
+    }
+
+    @Test
+    public void t3() throws IOException {
+        assertTrue(test.runTestWithName(4, "t3"));
+    }
+
+    @Test
+    public void t4() throws IOException {
+        assertTrue(test.runTestWithName(4, "t4"));
+    }
+
+    @Test
+    public void t5() throws IOException {
+        assertTrue(test.runTestWithName(4, "t5"));
+    }
+
+    @Test
+    public void t6() throws IOException {
+        assertTrue(test.runTestWithName(4, "t6"));
+    }
+
+    @Test
+    public void t7() throws IOException {
+        assertTrue(test.runTestWithName(4, "t7"));
+    }
+
+    @Test
+    public void t8() throws IOException {
+        assertTrue(test.runTestWithName(4, "t8"));
+    }
+
+    @Test
+    public void t9() throws IOException {
+        assertTrue(test.runTestWithName(4, "t9"));
+    }
+
+    @Test
+    public void t10() throws IOException {
+        assertTrue(test.runTestWithName(4, "t10"));
+    }
+
+    @Test
+    public void t11() throws IOException {
+        assertTrue(test.runTestWithName(4, "t11"));
+    }
+
+    @Test
+    public void t12() throws IOException {
+        assertTrue(test.runTestWithName(4, "t12"));
+    }
+
+    @Test
+    public void t13() throws IOException {
+        assertTrue(test.runTestWithName(4, "t13"));
+    }
+
+    @Test
+    public void t14() throws IOException {
+        assertTrue(test.runTestWithName(4, "t14"));
+    }
+
+    @Test
+    public void t15() throws IOException {
+        assertTrue(test.runTestWithName(4, "t15"));
+    }
+}
