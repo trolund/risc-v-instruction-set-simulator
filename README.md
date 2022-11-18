913346a82267e8b988f8c19f9ad96bbcf5774a6e
Troels Lund <trolund@gmail.com>
2022-11-18 22:31:40 +0100

    readme

--- README.md
+++ README.md
@@ -8,5 +8,28 @@ RV32I Instructions info:
 https://msyksphinz-self.github.io/riscv-isadoc/html/rvi.html#lbu
 https://itnext.io/risc-v-instruction-set-cheatsheet-70961b4bbe8
 
+## RV32I usage
 
-RISC-V is little-endian.
+The CLI can be used by using the compiled jar file *rv32i.jar* like this: 
+
+    java -cp "picocli-4.7.0.jar:rv32i.jar" RV32I
+
+The CLI have a number of options that can be seen by using the command:
+
+    RV32I --help 
+
+this will result in this info:
+
+    Usage: RV32I [-dhpruV] [-e=<ecall>] <path>
+    A basic RISC-V simulator. Supporting the RV32I instructions.
+    <path>            Path to the bin file containing the program.
+    -d, --debug           Will do debug printing. default: false.
+    -e, --ecall=<ecall>   The register that 'ecall' uses to decide the kind of
+    env call. default: a7.
+    -h, --help            Show this help message and exit.
+    -p, --print           Will print the 32 registers to the console after each
+    instruction. default: false.
+    -r, --result          Print the result of the 32 registers after execution.
+    default: true.
+    -u, --dump            Write data-dump file after execution. default: false.
+    -V, --version         Print version information and exit.
--- src/RV32I.java
+++ src/RV32I.java
@@ -1,9 +1,46 @@
+import Core.ISASimulator;
+import IO.ProgramLoader;
 import picocli.CommandLine;
 
-public class RV32I {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "RV32I", mixinStandardHelpOptions = true, version = "RV32I 1.0", description = "A basic RISC-V simulator. Supporting the RV32I instructions.")
+public class RV32I implements Callable<Integer> {
+
+    @CommandLine.Parameters(index = "0", description = "Path to the bin file containing the program.")
+    private String path;
+
+    @CommandLine.Option(names = {"-p", "--print"}, description = "Will print the 32 registers to the console after each instruction. default: false.")
+    private boolean print = false;
+
+    @CommandLine.Option(names = {"-d", "--debug"}, description = "Will do debug printing. default: false.")
+    private boolean debug = false;
+
+    @CommandLine.Option(names = {"-u", "--dump"}, description = "Write data-dump file after execution. default: false.")
+    private boolean dump = false;
+
+    @CommandLine.Option(names = {"-r", "--result"}, description = "Print the result of the 32 registers after execution. default: true.")
+    private boolean result = true;
+
+    @CommandLine.Option(names = {"-e", "--ecall"}, description = "The register that 'ecall' uses to decide the kind of env call. default: a7.")
+    private int ecall = 17;
+
+    @Override
+    public Integer call() throws IOException {
+        ProgramLoader loader = new ProgramLoader();
+        File file = new File(System.getProperty("user.dir") + this.path);
+        int[] progr = loader.readBinFile(file);
+        Path fileName = file.toPath().getFileName();
+        ISASimulator vm = new ISASimulator(fileName.getFileName().toString(), print, debug, dump, ecall, result);
+        vm.runProgram(progr);
+        return 0;
+    }
 
     public static void main(String... args) {
-        int exitCode = new CommandLine(new CLI()).execute(args);
+        int exitCode = new CommandLine(new RV32I()).execute(args);
         System.exit(exitCode);
     }
 
